e6bb8d0dcb05fe9907a9edecbb1f7eb522fe29e4
Jakob Mierl <jakob.mierl@outlook.com>
2023-02-13 11:32:54 +0100

    added autoclicker and css

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,6 +1,18 @@
-var clicks = 0;
+let clicks = 0,
+    tId;
 
 function onClick() {
     clicks += 1;
     document.getElementById("clicks").innerHTML = clicks;
+};
+
+function autoClick() {
+    clearInterval(tId);
+    setInterval(function(){
+        document.getElementById("clicks").innerHTML = clicks += 1;
+    }, 1000);
+};
+
+function stop(){
+    clearInterval(tId);
 };
